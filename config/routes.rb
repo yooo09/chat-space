4951c923170023f9d9448e5d84d554afe44adc65
yooo09 <65532756+yooo09@users.noreply.github.com>
2020-05-29 23:26:09 +0900

    インクリメンタルサーチ

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     #メッセージのルーティングはグループにネストした形になっている
     #メッセージの保存を行う:create
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
