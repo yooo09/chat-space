d6e0012e722e38e809fb5664b009dff4dd305b40
yooo09 <65532756+yooo09@users.noreply.github.com>
2020-06-03 17:28:22 +0900

    Unicornをインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   #バリデーションは空の文字列の場合にエラーが発生する
   validates :name, presence: true, uniqueness: true
   has_many :group_users
